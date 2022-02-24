16008f58500dee6adedd9f5689d7e83b109f1d5d
Ismael Santana <souz4ismael@gmail.com>
2022-02-24 13:03:06 -0300

    removed unsed imports

--- src/modules/customer/useCases/createCustomer/CreateCustomerUseCase.ts
+++ src/modules/customer/useCases/createCustomer/CreateCustomerUseCase.ts
@@ -1,9 +1,7 @@
 import { Customer } from "@modules/customer/domain/Customer";
 import { CreateCustomerDTO } from "@modules/customer/dtos/CreateCustomerDTO";
 import { CustomerMap } from "@modules/customer/mappers/CustomerMap";
-import { ICustomerRolesRepository } from "@modules/customer/repositories/ICustomerRolesRepository";
 import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
-import { IRolesRepository } from "@modules/customer/repositories/IRolesRepository";
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 import { CreateCustomerErrors } from "./CreateCustomerErrors";
@@ -13,10 +11,6 @@ class CreateCustomerUseCase {
   constructor(
     @inject("CustomersRepository")
     private readonly customersRepository: ICustomersRepository,
-    // @inject("RolesRepository")
-    // private readonly rolesRepository: IRolesRepository,
-    // @inject("CustomerRolesRepository")
-    // private readonly customerRolesRepository: ICustomerRolesRepository
   ) {}
 
   async execute({ email, name, password }: CreateCustomerDTO) {
@@ -52,3 +46,4 @@ class CreateCustomerUseCase {
 }
 
 export { CreateCustomerUseCase };
+
