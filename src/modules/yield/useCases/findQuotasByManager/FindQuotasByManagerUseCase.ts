9981904d53b65dcf16eb99bcf0f25a822d256130
Ismael Santana <souz4ismael@gmail.com>
2022-03-02 13:41:56 -0300

    inject repositoryy

--- src/modules/yield/useCases/findQuotasByManager/FindQuotasByManagerUseCase.ts
+++ src/modules/yield/useCases/findQuotasByManager/FindQuotasByManagerUseCase.ts
@@ -1,11 +1,15 @@
 import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
 import { Quota } from "@modules/yield/domain/Quota";
 import { IQuotasRepository } from "@modules/yield/repositories/IQuotasRepository";
+import { inject, injectable } from "tsyringe";
 import { FindQuotasErrors } from "./FindQuotasByManagerErrors";
 
+@injectable()
 class FindQuotasByManagerUseCase {
   constructor(
+    @inject("CustomersRepository")
     private readonly customersRepository: ICustomersRepository,
+    @inject("QuotasRepository")
     private readonly quotasRepository: IQuotasRepository
   ) {}
 
@@ -20,7 +24,7 @@ class FindQuotasByManagerUseCase {
 
     const quotasDB = await this.quotasRepository.findByManagerId(managerId);
 
-    quotas = quotasDB.map((quota, index) => {
+    quotas = quotasDB.map((quota) => {
       const quotaDomain = new Quota();
 
       Object.assign(quotaDomain, quota);
